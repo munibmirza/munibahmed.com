8c06e90fe0de67fa5876d78d80e1f04565bbaf51
munibmirza <munibahmed125@gmail.com>
2026-06-28 15:39:01 +0500

    Clean up repo: remove dev scripts, backups, build artifacts, and stray data file

--- InterAudit-P1.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "" pythonw "%~dp0main.py"
--- inspect2.py
+++
@@ -1,7 +0,0 @@
-c = open('InterAudit-P1.html', encoding='utf-8').read()
-
-# Find > prompt near end of body
-body_end = c.rfind('</body>')
-last_section = c[body_end-3000:body_end]
-print('=== last 3000 chars before </body> ===')
-print(repr(last_section[:2000]))
